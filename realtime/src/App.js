08f6b77ae565a1b6003e2589f00a8db65614f8c7
Juni-Devs <38501366+HoupSS@users.noreply.github.com>
2018-11-28 01:32:32 +0300

    React Js
    Çalışma

---
+++ realtime/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {Islemler} from './Components/Islemler'
+class App extends Component {
+  render() {
+    return (
+<Islemler></Islemler>
+    );
+  }
+}
+
+export default App;
