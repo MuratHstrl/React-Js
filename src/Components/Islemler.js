0e3d945d91849c89d0230daa381c5d28928e9592
Juni-Devs <38501366+HoupSS@users.noreply.github.com>
2018-11-28 11:02:51 +0300

    Update
    *Arama Yeri Eklendi
    *Hatalar Giderildi

--- src/Components/Islemler.js
+++ src/Components/Islemler.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import firebase from 'firebase'
+
+// Arama Yeri için
+function Arama(term) {
+    String.prototype.turkishToLower = function () {
+        var string = this;
+        var letters = { "İ": "i", "I": "ı", "Ş": "ş", "Ğ": "ğ", "Ü": "ü", "Ö": "ö", "Ç": "ç" };
+        string = string.replace(/(([İIŞĞÜÇÖ]))/g, function (letter) { return letters[letter]; })
+        return string.toLowerCase();
+    }
+    return function (x) {
+        return x.Dataname.turkishToLower().includes(term.turkishToLower()) || !term;
+    }
+}
 export class Islemler extends React.Component{
 constructor(props){
     super(props);
@@ -7,12 +20,15 @@ constructor(props){
         data:' ',
         // Veritabanındaki bilgiler her zaman Dizi şeklinde gelir
         Datass:[],
-
-        Gdata:' '
+        Gdata:' ',
+        AramaKelimesi: ''
     }
     this.dara=this.dara.bind(this);
     this.datadelete = this.datadelete.bind(this);
 }
+
+    
+
   /*  Dara(){
         const inp = document.getElementById('Datainput');
         const val = inp.value;
@@ -91,6 +107,28 @@ this.setState({Gdata:e.target.value})
             )
         })
  //*************************************-
+
+ // Arama yapmak için
+ const Veriler = this.state.Datass.filter(Arama(this.state.AramaKelimesi)).map((deger, i) => {
+     return(
+        <div >
+        <p key={i} class="list-group-item" style={{borderRadius:'10px',backgroundColor:'#dcdde1', textAlign:"center"}}> <h4>{deger.Dataname} </h4> 
+        <div class='ui focus input'>
+        <input type='text' placeholder='GData' id='GDatainput' onChange={this.GDara} value={this.state.Gdata}/>
+        <br/>
+        </div>
+        
+        <center>
+         <button class='ui red inverted button' onClick={() => {this.datadelete(deger.key)}}>Sil</button>
+         <button class="ui yellow button" onClick={()=>{this.DataUpdate(deger.key)}}>Güncelle</button>
+         </center>            
+         <br/>        
+        </p> 
+        <br/>
+        </div>   
+     )
+ })
+ //------------------------------------------------------
         return(
         <div style={{width:'25%'}}>
                     <div class='ui card'>
@@ -117,13 +155,13 @@ this.setState({Gdata:e.target.value})
                     <hr/>    
                     <center> 
                    <div class="ui icon input">                   
-                   <input type="text" placeholder="Ara" id='DataSearch'/>
+                   <input type="text" placeholder="Ara" id='DataSearch' onChange={(e) => { this.setState({ AramaKelimesi: e.target.value }) }} value={this.state.AramaKelimesi}/>
                     <i class="inverted circular search link icon"></i>                                                                   
                  </div>
                  </center> 
                     <hr/>
                     <div style={{height: '450px', overflow: 'scroll', width: '100%'}}>                  
-                        {x}                       
+                        {Veriler}                       
                     </div>
                     </div>
                     </div>
